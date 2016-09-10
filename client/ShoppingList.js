070d50016125c28fe3fb371e38829475f6ce29e3
Hakan Mehmed <hakanmehmed@Hakans-MacBook-Pro.local>
2016-09-10 21:19:26 +0100

    shopping list integration

---
+++ client/ShoppingList.js
@@ -0,0 +1,13 @@
+// Template level subscribtion
+Template.ShoppingList.onCreated(function() {
+	var self = this;
+	self.autorun(function() {
+		self.subscribe('recipes');
+	});
+});
+
+Template.ShoppingList.helpers({
+	shoppingList: () => {
+		return Recipes.find({inMenu: true});
+	}
+});
